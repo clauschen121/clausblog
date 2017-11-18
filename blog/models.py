c40036e0b2ff45d8d2d940923f40259a5c127fcd
claus chen <245618722@qq.com>
2017-11-18 23:03:08 +0800

    add artical,tag,category models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+    '''
+    文章分类：字段为name
+    '''
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    '''
+    文章标签：字段为name
+    '''
+    name = models.CharField(max_length=100)
+
+
+class Article(models.Model):
+    '''
+    文章内容结构
+    标题，内容，创建时间，修改时间，摘要，图片，分类，标签，作者
+    '''
+
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    image = models.CharField(max_length=100)
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
